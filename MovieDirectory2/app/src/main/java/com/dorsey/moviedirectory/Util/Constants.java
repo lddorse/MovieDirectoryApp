d2e36e9f02067ac6fcd0229600db008a418f1363
leo <lddorse@gmail.com>
2020-01-13 21:50:02 -0600

    Repo Update

---
+++ MovieDirectory2/app/src/main/java/com/dorsey/moviedirectory/Util/Constants.java
@@ -0,0 +1,10 @@
+package com.dorsey.moviedirectory.Util;
+
+public class Constants {
+    public static final String URL_LEFT = "http://www.omdbapi.com/?s=";
+    public static final String URL = "http://www.omdbapi.com/?i=";
+    //public static final String API_KEY = "&apikey=f0f386a2";
+    public static final String API_KEY = "&apikey=cdc4cef5";
+    public static final String URL_RIGHT = "&page=2";
+
+}
